00263d6c1568176fae5147a2ce6a70795e10f344
nikhil <nsingh@cdac.in>
2025-08-08 23:00:04 +0530

    Shadcn dashboard added

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import type { JSX } from 'react';
-import Dashboard from './pages/AppSideBar'; 
+import SidebarV3 from './dashboard/page';
 
 const App = (): JSX.Element => (
     <Routes>
-        <Route path="/" element={<Dashboard/>}>
+        <Route path="/" element={<SidebarV3/>}>
       </Route>
     </Routes>
 );
